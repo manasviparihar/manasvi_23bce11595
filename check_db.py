1f2cbed278f723d882262668f08831fa23f385dc
Manasvi Parihar <manasvi.23bce11595@vitbhopal.ac.in>
2025-06-18 03:26:45 +0530

    Delete check_db.py

--- check_db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["medical_db"]
-collection = db["records"]
-
-records = list(collection.find({}, {'_id': 0}))
-print("Records in DB:", records)
-
